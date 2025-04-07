1b812708164b6b9f89c0e2f2abdcc8b317c16b7e
stivenzp <stiven.zapata@est.iudigital.edu.co>
2025-04-06 22:04:34 -0500

    Update axios-config.js

--- src/Helper/axios-config.js
+++ src/Helper/axios-config.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://backendmovies-nxr4.onrender.com'
+    //baseURL: 'http://localhost:4000/'
 });
 
 export{
